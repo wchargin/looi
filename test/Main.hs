af1ef9e1cf7612decf3e52352ce4c1b39e724bef
William Chargin <wchargin@gmail.com>
2015-11-08 14:01:36 -0800

    Switch to automatic test discovery

--- test/Main.hs
+++ test/Main.hs
@@ -1,1 +1,19 @@
-main = putStrLn "Hello, tests!"
+-- Automatically discover *Spec.hs files
+-- with a GCC pre-processor directive.
+-- See http://hspec.github.io/hspec-discover.html for more information.
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+
+-- The explicit form looks like this...
+{-
+import Test.Hspec
+
+import qualified SExpTest
+-- ...
+
+tests = [ describe "SExp" SExpTest.spec
+        -- ...
+        ]
+
+main :: IO ()
+main = hspec $ sequence_ tests
+-}
