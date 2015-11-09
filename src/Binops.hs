47af39f261c3dbfd398d676aee4afeab51176b88
William Chargin <wchargin@gmail.com>
2015-11-08 22:54:29 -0800

    Shorten redundant binop names

--- src/Binops.hs
+++ src/Binops.hs
@@ -9,22 +9,22 @@ numericBinop :: (NumericValue -> NumericValue -> NumericValue) -> Binop
 numericBinop op (NumV a) (NumV b) = Right $ NumV (a `op` b)
 numericBinop _ _ _ = typeError "expected both operands to be numeric values"
 
-binopDivide :: Value -> Value -> Either String Value
-binopDivide (NumV _) (NumV 0) = Left "division by zero"
-binopDivide (NumV a) (NumV b) = Right $ NumV $ a `div` b
-binopDivide _ _ = typeError "expected both operands to be numeric values"
+divide :: Value -> Value -> Either String Value
+divide (NumV _) (NumV 0) = Left "division by zero"
+divide (NumV a) (NumV b) = Right $ NumV $ a `div` b
+divide _ _ = typeError "expected both operands to be numeric values"
 
-binopEqHuh :: Value -> Value -> Either String Value
-binopEqHuh (NumV a) (NumV b)    = Right $ BoolV $ a == b
-binopEqHuh (BoolV a) (BoolV b)  = Right $ BoolV $ a == b
-binopEqHuh _ _                  = Right $ BoolV False
+eqHuh :: Value -> Value -> Either String Value
+eqHuh (NumV a) (NumV b)    = Right $ BoolV $ a == b
+eqHuh (BoolV a) (BoolV b)  = Right $ BoolV $ a == b
+eqHuh _ _                  = Right $ BoolV False
 
 binops :: [(Identifier, Binop)]
 binops = [ ("+", numericBinop (+))
          , ("-", numericBinop (-))
          , ("*", numericBinop (*))
-         , ("/", binopDivide)
-         , ("eq?", binopEqHuh)
+         , ("/", divide)
+         , ("eq?", eqHuh)
          ]
 
 applyBinop :: Identifier -> Value -> Value -> Either String Value
