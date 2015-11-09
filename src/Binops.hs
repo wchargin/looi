637e8a821b899ecebcbd15f2b3f99546d0311bd8
William Chargin <wchargin@gmail.com>
2015-11-09 07:02:31 -0800

    Specialize `Binops.typeError` to numeric types
    …because that's the only way it's ever used.

--- src/Binops.hs
+++ src/Binops.hs
@@ -2,17 +2,18 @@ module Binops(binops, applyBinop) where
 
 import CoreTypes
 
-typeError :: String -> Either String b
-typeError = Left . ("type error: " ++)
+typeErrorExpectedNumeric :: Either String b
+typeErrorExpectedNumeric = Left msg
+    where msg = "type error: expected both operands to be numeric values"
 
 numericBinop :: (NumericValue -> NumericValue -> NumericValue) -> Binop
 numericBinop op (NumV a) (NumV b) = Right $ NumV (a `op` b)
-numericBinop _ _ _ = typeError "expected both operands to be numeric values"
+numericBinop _ _ _ = typeErrorExpectedNumeric
 
 divide :: Value -> Value -> Either String Value
 divide (NumV _) (NumV 0) = Left "division by zero"
 divide (NumV a) (NumV b) = Right $ NumV $ a `div` b
-divide _ _ = typeError "expected both operands to be numeric values"
+divide _ _ = typeErrorExpectedNumeric
 
 eqHuh :: Value -> Value -> Either String Value
 eqHuh (NumV a) (NumV b)    = Right $ BoolV $ a == b
@@ -21,7 +22,7 @@ eqHuh _ _                  = Right $ BoolV False
 
 leq :: Value -> Value -> Either String Value
 leq (NumV a) (NumV b) = Right $ BoolV $ a <= b
-leq _ _               = typeError "expected both operands to be numeric values"
+leq _ _               = typeErrorExpectedNumeric
 
 binops :: [(Identifier, Binop)]
 binops = [ ("+", numericBinop (+))
