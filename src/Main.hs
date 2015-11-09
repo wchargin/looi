0a8224a36bc1b4ab4da9d55a3224ef7fdc3a6b3f
William Chargin <wchargin@gmail.com>
2015-11-09 06:48:46 -0800

    Create a main function

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,18 @@
-main = putStrLn "Hello, world!"
+-- Simple entry point around the interpreter.
+-- Reads a full program from stdin,
+-- then parses and evaluates it,
+-- printing the result to stdout or an error to stderr.
+module Main(main) where
+
+import System.Exit
+import System.IO
+
+import CoreTypes
+import Interpreter (topEval)
+
+printResult :: Either String Value -> IO ()
+printResult (Left err) = hPutStrLn stderr err >> exitFailure
+printResult (Right val) = print val >> exitSuccess
+
+main :: IO ()
+main = getContents >>= (printResult . topEval)
