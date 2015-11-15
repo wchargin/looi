62445c6afa02302bda00d59fbb78e9d5aad6abda
William Chargin <wchargin@gmail.com>
2015-11-14 21:47:42 -0800

    Add the actual store (not yet used)

--- src/CoreTypes.hs
+++ src/CoreTypes.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE FlexibleContexts, TupleSections #-}
+
 module CoreTypes where
 
 import Control.Monad.State (State, gets, modify)
 import Control.Monad.Except (Except)
+import Control.Monad.State.Class (MonadState, get, gets, put)
 
 import qualified Data.Map as Map
 
@@ -25,6 +28,10 @@ data ExprC = ValueC Value
            | IfC ExprC ExprC ExprC
            deriving (Eq, Show)
 
+--------------------------------------------------------------
+-- The environment
+--------------------------------------------------------------
+
 type Environment = Map.Map Identifier Value
 
 emptyEnvironment :: Environment
@@ -36,22 +43,6 @@ envBind = Map.insert
 envLookup :: Identifier -> Environment -> Maybe Value
 envLookup = Map.lookup
 
-type Address = Int
-type Store = Map.Map Address Value
-type StoreOp α = State Store α
-
-emptyStore :: Store
-emptyStore = Map.empty
-
-storeLookup :: Address -> StoreOp (Maybe Value)
-storeLookup a = gets $ Map.lookup a
-
-allocate :: Value -> StoreOp Address
-allocate v = do
-    addr <- gets Map.size
-    modify $ Map.insert addr v
-    return addr
-
 -- Convert a value to a string to be printed.
 -- Note that you can also just `show` the value,
 -- which will retain more information
@@ -62,3 +53,38 @@ serialize (NumV n) = show n
 serialize (BoolV b) = if b then "true" else "false"
 serialize (ArrayV _ _) = "#<array>"
 serialize ClosureV{} = "#<procedure>"
+
+
+--------------------------------------------------------------
+-- The store
+--------------------------------------------------------------
+
+-- Look up a value in the store based on its address.
+-- Return Nothing if the specified address is not allocated.
+storeLookup :: MonadState Store m => Address -> m (Maybe Value)
+storeLookup addr = gets $ Map.lookup addr . contents
+
+type Address = Int
+data Store = Store { contents :: Map.Map Address Value
+                   , nextAddress :: Address
+                   }
+                   deriving (Show, Eq)
+type StoreOp α = State Store α
+
+emptyStore :: Store
+emptyStore = Store Map.empty 0
+
+-- Allocate a bunch of blocks in the store,
+-- initializing each one to the given value,
+-- and return the starting address.
+allocateMany :: MonadState Store m => Int -> Value -> m Address
+allocateMany num val = do
+    Store old addr <- get
+    let new = Map.union (Map.fromList $ map (, val) $ take num [addr..]) old
+    put $ Store new (addr + num)
+    return addr
+
+-- Allocate a single block in the store with the given value,
+-- and return its new address.
+allocate :: MonadState Store m => Value -> m Address
+allocate = allocateMany 1
